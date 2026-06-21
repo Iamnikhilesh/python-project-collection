ec2570de2c0314448766a7552e2a3dda502700a1
Nikhilesh Anamanla <nikhileshanamanla@gmail.com>
2026-06-21 17:38:25 +0530

    Add files via upload

---
+++ Quiz_Game/quiz_game.py
@@ -0,0 +1,40 @@
+print("Welcome to quiz game!")
+
+playing = input("Do want to play? ")
+
+if playing.lower() != "yes":
+    quit("you are exited")
+
+print("Okay! Let`s play!")
+score = 0
+
+answer = input("What does CPU stands for? ")
+if answer.lower() == "central processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stands for? ")
+if answer.lower() == "graphics processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stands for? ")
+if answer.lower() == "random access memory ":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stands for? ")
+if answer.lower() == "power supply":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " question correct! ")
+print("You got " + str((score / 4) * 100) + " %. ")
